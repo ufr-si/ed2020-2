4c40350d0559c2b31f3173b64cfb8b106df305ac
Cleyton Slaviero <cslaviero@gmail.com>
2021-05-04 02:29:55 +0000

    lista_com_busca_binaria

--- lista-sequencial/lista-sequencial.py
+++ lista-sequencial/lista-sequencial.py
@@ -1,6 +1,6 @@
 # o que vamos fazer ? 
 # lista-sequencial 
-
+import math
 # inicializar estrutura
 def inicializa_lista(n):
    return [None] * n
@@ -66,20 +66,32 @@ def del_elemento(n):
 
     pass     
 
-def busca_elemento(n):
-    #percorre até achar    
-    pass
-
-def busca_melhor(n):
-    pass
+def busca_elemento(lista,n):
+    #percorre até achar
+    for i in range (0,len(lista)):
+        if(lista[i] == n):
+            return i
+    return -1
 
 ### RESTRICAO: lista tem que estar ordenada para esse funcionar
-
 # o que é ? 
 # como se comporta? 
-def busca_binaria(n):
-    # 
-    pass
+#lista_telefonica!
+def busca_binaria(lista,x):
+    inf = 0
+    sup = len(lista)-1
+    while inf <= sup:
+        calc = inf + sup /2
+        meio = math.floor(calc)
+        if lista[meio] == x:
+            return meio
+        else:
+            if lista[meio] < x:
+                inf = meio +1
+            else:
+                # lista[meio] > x
+                sup = meio-1
+    return -1
 
 # o que mais podemos fazer? 
 # inverter
