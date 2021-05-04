d4aa486094c5e56e47261c274c1de20551159d84
Cleyton Slaviero <cslaviero@gmail.com>
2021-05-04 01:54:15 +0000

    deque para o futuro

--- lista-sequencial/deque.py
+++ lista-sequencial/deque.py
@@ -1,10 +1,8 @@
 #deque (Double Ended Queue)
-# insere e remove do começo ou do fim
+# insere e remove do começo ou do fim 
+# fica para explicar com lista (duplamente) encadeada :-)
 
 deque = [None]*5
-
-
-
 
 def add(n,pos):
     pass
