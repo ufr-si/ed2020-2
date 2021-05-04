814950e50564249829d337fadbeab914b9ffd378
Cleyton Slaviero <cslaviero@gmail.com>
2021-05-04 01:04:12 +0000

    fila finalizada e corrigida

--- lista-sequencial/fila.py
+++ lista-sequencial/fila.py
@@ -7,61 +7,69 @@ fila = [None]*4
 #  , , , . 
 primeiro = 0
 ultimo = 0
-qtde = 0
+total = 0
 
 def queue(n):
+    print("tentando adicionar ",n)
     global ultimo
-    global capacidade
     global fila
-    if(qtde == len(fila)):
-        print("ERRO, FILA CHEIA")
-        return -1
-    else:
-        fila[ultimo]=n
-        qtde = qtde +1 
-        #alterar o valor de ultimo
+    global total
+
+    if(total != len(fila)):
+        fila[ultimo] = n
+        total = total +1
+
         if ultimo == len(fila)-1:
             ultimo = 0
         else:
-            ultimo = ultimo +1
-        return 0
+            ultimo = ultimo + 1        
+    else:
+        print("ERRO, FILA CHEIA")
 
 def dequeue():
+    print("removendo")
     global primeiro
-    global capacidade
     global fila
-    if capacidade > 0: # posso remover ? tem elemento?
-        elem = fila[primeiro] #removo
+    global total
+    elem = None
+    if(total != 0):
+            
+        elem = fila[primeiro]
         fila[primeiro] = None
-        capacidade = capacidade - 1
-        if primeiro +1 > len(fila)-1: # se eu tiver jogando muito pra frente
-            primeiro=0 # volto pro zero 
+        total = total - 1 
+        if primeiro == len(fila) -1: 
+            primeiro = 0
         else:
             primeiro = primeiro +1
     else:
-        print("ERRO, FILA VAZIA!")
-        return None
-    return elem 
-    pass
+        print("Fila VAZIA")
+        
+    return elem
+        
 
-
+#zona de testes
 queue("a")
 print(fila)
-queue("b")
+queue("a")
 print(fila)
-queue("c")
+queue("a")
 print(fila)
-queue("d")
+queue("a")
+print(fila)
+queue("a")
 print(fila)
 
-print(dequeue())
+print("removi: ",dequeue())
 print(fila)
-print(dequeue())
+print("removi: ",dequeue())
 print(fila)
-print(dequeue())
+print("removi: ",dequeue())
 print(fila)
-print(dequeue())
+print("removi: ",dequeue())
 print(fila)
-dequeue()
-dequeue()
-dequeue()
+print("removi: ",dequeue())
+print(fila)
+
+print("primeiro, ",primeiro)
+print("ultimo, ",ultimo)
+print("total, ",total)
